6c6ef119bf8764abe8efedbc633c8e9f1e8a2a58
Janith Leanage <janithleanage@gmail.com>
2018-05-25 20:38:03 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## LK-DataExplorer
+
+A small React project to view and explore the data available through data.gov.lk
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Welcome to LK DataExplorer</h1>
+    </header>
+    <p className="App-intro">We will be getting started soon</p>
+  </div>
+);
+
+export default App;
