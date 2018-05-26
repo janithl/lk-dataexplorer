c9cafceeabd1004649a3c9a5f4ad21abf663ad18
Janith Leanage <janithleanage@gmail.com>
2018-05-26 16:41:29 +0530

    quick and dirty viewer logic

--- src/explorer/ui/Viewer.js
+++ src/explorer/ui/Viewer.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Button } from "reactstrap";
+import { Button, Table } from "reactstrap";
 
 import { fetchDataset } from "../actions";
 
-const Viewer = ({ id, onRefresh }) => (
-  <Button onClick={onRefresh(id)}>Refresh</Button>
+const Viewer = ({ explorer, onRefresh }) => (
+  <div>
+    <Button onClick={onRefresh}>Refresh</Button>
+
+    <Table striped>
+      <tbody>
+        {explorer &&
+          Object.values(explorer).map(item => (
+            <tr key={item["polling-division"]}>
+              <th scope="row">{item["polling-division"]}</th>
+              {Object.values(item).map(cell => <td>{cell}</td>)}
+            </tr>
+          ))}
+      </tbody>
+    </Table>
+  </div>
 );
 
-const mapStateToProps = () => ({});
+const mapStateToProps = state => ({ explorer: state.explorer.electoral });
 
-const mapDispatchToProps = dispatch => ({
-  onRefresh: id => dispatch(fetchDataset(id))
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onRefresh: id => dispatch(fetchDataset(ownProps.id))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Viewer);
